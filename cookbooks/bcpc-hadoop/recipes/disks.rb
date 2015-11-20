7df2ab70c2f1baeb8726ef906b6a033e55405e43
Lev Bronshtein <lbronshtein@bloomberg.net>
2015-11-20 13:02:23 -0500

    mounting extra disk on user directory

--- cookbooks/bcpc-hadoop/recipes/disks.rb
+++ cookbooks/bcpc-hadoop/recipes/disks.rb
@@ -1,3 +1,5 @@
+require "securerandom"
+
 package "xfsprogs" do
   action :install
 end
@@ -9,8 +11,18 @@ directory "/disk" do
   action :create
 end
 
-if node[:bcpc][:hadoop][:disks].length > 0 then
-  node[:bcpc][:hadoop][:disks].each_index do |i|
+user_disk = nil
+hadoop_disks = node[:bcpc][:hadoop][:disks]
+# have we reserved a disk for user directories
+if node[:bcpc][:hadoop][:user_disk] == true then
+  # perhaps instead of doing array slicing math, we explicitly specify
+  # a disk to be used as a user partition as en environment var
+  user_disk =  node[:bcpc][:hadoop][:disks][-1]
+  hadoop_disks = node[:bcpc][:hadoop][:disks][0..-2]
+end
+
+if hadoop_disks.length != nil then
+  doop_disks.each_index do |i|
     directory "/disk/#{i}" do
       owner "root"
       group "root"
@@ -19,7 +31,7 @@ if node[:bcpc][:hadoop][:disks].length > 0 then
       recursive true
     end
    
-    d = node[:bcpc][:hadoop][:disks][i]
+    d = hadoop_disks
     execute "mkfs -t xfs -f /dev/#{d}" do
       not_if "file -s /dev/#{d} | grep -q 'SGI XFS filesystem'"
     end
@@ -31,7 +43,24 @@ if node[:bcpc][:hadoop][:disks].length > 0 then
       action [:enable, :mount]
     end
   end
-  node.set[:bcpc][:hadoop][:mounts] = (0..node[:bcpc][:hadoop][:disks].length-1).to_a
+  node.set[:bcpc][:hadoop][:mounts] = (0..hadoop_disks.length-1).to_a
 else
-  Chef::Application.fatal!('Please specify some node[:bcpc][:hadoop][:disks]!')
+  Chef::Application.fatal!('Please specify enough disks in node[:bcpc][:hadoop][:disks]!')
+end
+  
+# create the user disk
+if node[:bcpc][:hadoop][:user_disk] == true then
+  if user_disk != nil then
+    uuid_label = SecureRandom.uuid
+    execute "mkfs -t ext4 -U #{uuid_label} /dev/#{user_disk}" do
+      not_if "file -s /dev/#{user_disk} | grep -q 'ext4 filesystem data'"
+    end
+    quotaenable "user_dir_quota" do
+      mount_pount "/home"
+      mount_device "UUID=#{uuuid_label}"  
+      quota_type :group
+    end
+  else
+    Chef::Application.fatal!('Please specify enough disks in node[:bcpc][:hadoop][:disks]!')
+  end
 end
