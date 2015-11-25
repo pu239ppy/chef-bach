3ad8181b6f5867437b5ef35abfed9870af12e4da
Lev Bronshtein <lbronshtein@bloomberg.net>
2015-11-25 08:07:07 -0500

    for now removing fsquota

--- cookbooks/fsquota/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'fsquota'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures fsquota'
-long_description 'Installs/Configures fsquota'
-version '0.1.0'
-
