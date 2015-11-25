5738f4258cb721040f819b4b7ae34b75e4c30711
Lev Bronshtein <lbronshtein@bloomberg.net>
2015-11-24 20:32:16 -0500

    metadata deps

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "bcpc", ">= 0.5.0"
+depends "fsquota"
 depends "dpkg_autostart"
 depends "sysctl"
 depends "pam"
