dc6dc4f55db3cd578778ca252d1944669a60c19b
Lev Bronshtein <lbronshtein@bloomberg.net>
2016-04-18 17:19:22 -0400

    just get form master

--- setup_bach_env.sh
+++ setup_bach_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # download external environment -- roles, cluster.txt and environments
-[[ -d external_environment ]] || git clone https://github.com/pu239ppy/chef-bach-env.git -b no_roles_environment_cluster_text external-environment 
+[[ -d external_environment ]] || git clone https://github.com/bloomberg/chef-bach-env.git external-environment 
 
 ln -s external_environment/environments
 ln -s external_environment/cluster.txt
