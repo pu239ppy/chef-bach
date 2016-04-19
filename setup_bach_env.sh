0428a0198a7cf81e771037aacf76b8909832de85
Lev Bronshtein <lbronshtein@bloomberg.net>
2016-04-19 10:54:38 -0400

    introduce external environment (roles, cluster.txt, environments) as a submodule

--- setup_bach_env.sh
+++ setup_bach_env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 # download external environment -- roles, cluster.txt and environments
-[[ -d external_environment ]] || git clone https://github.com/bloomberg/chef-bach-env.git external-environment 
+#[[ -d external_environment ]] || git clone https://github.com/bloomberg/chef-bach-env.git external-environment 
 
+git submodule add https://github.com/bloomberg/chef-bach-env.git --name external-environment -- external-environment
 ln -s external_environment/environments
 ln -s external_environment/cluster.txt
 ln -s external_environment/roles
