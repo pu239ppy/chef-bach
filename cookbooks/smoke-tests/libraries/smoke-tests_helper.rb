b8d5bbe3257c24a83ebf2e33747186bb3d0c0c5b
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-01-06 08:05:01 -0500

    smoke tests will have own cook book

---
+++ cookbooks/smoke-tests/libraries/smoke-tests_helper.rb
@@ -0,0 +1,33 @@
+# vim: tabstop=2:shiftwidth=2:softtabstop=2 
+module HadoopSmokeTests
+  module OozieHelper
+    def oozie_running?(host)
+      oozie_url = "oozie admin -oozie http://#{host}:11000/oozie -status"
+      cmd = Mixlib::ShellOut.new( oozie_url, :timeout => 20).run_command
+      Chef::Log.debug("Oozie status: #{cmd.stdout}")
+      cmd.exitstatus == 0 && cmd.stdout.include?('NORMAL')
+    end
+
+    def submit_workflow(host, prop_file)
+      oozie_cmd = "sudo -u #{user} oozie job -run -config #{prop_file}"
+      cmd = Mixlib::Shellout.new(oozie_cmd, timeout: 20).run_command
+      if cmd.exitstatus == 0
+        Chef::Log.debug("Job submission result: #{cmd.stdout}")
+      else
+        # raise exception?
+        Chef::Log.error("Job submission result: #{cmd.stderr}")
+      end
+      cmd.exitstatus
+    end
+
+    def submit_workflow_running_host(prop_file)
+      operational_hosts =
+        node[:bcpc][:hadoop][:oozie_hosts].select do 
+          |oozie_host| oozie_running?(oozie_host) 
+        end
+      if operational_hosts.length > 0 then
+        submit_workflow(operationl_hosts[0], user, prop_file)
+      end
+    end
+  end
+end
--- cookbooks/smoke-tests/recipes/oozie_smoke_test.rb
+++ cookbooks/smoke-tests/recipes/oozie_smoke_test.rb
@@ -1,7 +1,7 @@
 # vim: tabstop=2:shiftwidth=2:softtabstop=2 
 #
-# Cookbook Name:: bcpc
-# Recipe:: graphite
+# Cookbook Name:: smoke-test
+# Recipe:: oozie_smoke_test
 #
 # Copyright 2016, Bloomberg Finance L.P.
 #
@@ -17,12 +17,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Collect all we need to generate a job.properties
+# This will need to eventually be decoupled from bcpc-hadop
+
+test_user = node[:hadoop_smoke_tests][:oozie_user]
+workflow_path = node[:hadoop_smoke_tests][:hdfs_wf_path]
+
 ruby_block "collect_properties_data" do
   block do
     chef_env = node.environemnt
     krb_realm = node[:bcpc][:hadoop][:kerberos][:realm]
     resource_managers = node[:bcpc][:hadoop][:rm_hosts].map do |rms| rms.hostname end 
-    zookeper_quorum = node[:bcpc][:hadoop][:zookeeper][:servers].map do |zks| zks.hostname end
+    zookeeper_quorum = node[:bcpc][:hadoop][:zookeeper][:servers].map do |zks| zks.hostname end
     fs = "hdfs://#{chef_env}"
     rm = if resource_managers.length > 1 then chef_env else resource_managers[0] end
     thrift_uris = node[:bcpc][:hadoop][:hive_hosts]
@@ -31,20 +37,33 @@ ruby_block "collect_properties_data" do
     node.run_state['smoke']['fs'] = fs
     node.run_state['smoke']['thrift_uris'] = thrift_uris
     node.run_state['smoke']['krb_realm'] = krb_realm
+    node.run_state['smoke']['zk_quorum'] = zookeeper_quorum
   end
 end
 
-template '/etc/hadoop/conf/oozie-smoke-test/workflow.xml' do
+template "#{Chef::Config['file_cache_path']}/oozie-smoke-test/workflow.xml" do
   source 'smoke_test_xml.erb'
 end
 
-template '/etc/hadoop/conf/oozie-smoke-test/smoke_test_job.properties' do
+template "#{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties" do
   source 'smoke_test_job_properties.erb'
-  variables ( smoke: node.run_state['smoke'] )
+  variables ( {smoke: node.run_state['smoke']} )
+end
+
+execute "create HDFS workflow path #{workflow_path}" do
+  command "hdfs dfs -mkdir -p #{workflow_path} "\
+    && "hdfs dfs -chown -r #{test_user} #{workflow_path}"
+  user 'hdfs'
+end
+
+execute "upload workflow to #{workflow_path}" do
+  command "hdfs dfs -copyFromLocal #{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties #{workflow_path}" 
+  user test_user
 end
 
 ruby_block 'submit oozie smoke test' do
   block do
+    submit_workflow_running_host("#{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties")
   end
+  user test_user
 end
- 
