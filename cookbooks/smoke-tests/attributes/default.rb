a484b965625bfa6cf990bc6de107bf788e792382
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-01-13 22:26:13 -0500

    adding attributes

--- cookbooks/smoke-tests/attributes/default.rb
+++ cookbooks/smoke-tests/attributes/default.rb
@@ -2,3 +2,9 @@
 default['hadoop_smoke_tests'] = {}
 default['hadoop_smoke_tests']['oozie_user'] = 'ubuntu'
 default['hadoop_smoke_tests']['hdfs_wf_path'] = '/user/ubuntu/oozie-smoke-tests'
+default['hadoop_smoke_tests']['secure_cluster'] = true
+default['hadoop_smoke_tests']['krb5_realm'] = 'BCPC.EXAMPLE.COM'
+default['hadoop_smoke_tests']['rm'] = 'Test-Laptop'
+default['hadoop_smoke_tests']['fs'] = 'hdfs://Test-Laptop'
+default['hadoop_smoke_tests']['thrift_uris'] = ''
+default['hadoop_smoke_tests']['zk_quorum'] = ''
