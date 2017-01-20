3f4724a5d0d5c4193657bdba6c46f6c0a623e11d
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-01-20 14:16:25 -0500

    creted defaults to be fed into the template and recipe, this will give an opportunity to override with a wrapper

--- cookbooks/smoke-tests/recipes/oozie_smoke_test.rb
+++ cookbooks/smoke-tests/recipes/oozie_smoke_test.rb
@@ -1,4 +1,4 @@
-# vim: tabstop=2:shiftwidth=2:softtabstop=2 
+# vim: tabstop=-1:shiftwidth=2:softtabstop=2 
 #
 # Cookbook Name:: hadoop-smoke-tests
 # Recipe:: oozie_smoke_test
@@ -23,25 +23,25 @@
 test_user = node[:hadoop_smoke_tests][:oozie_user]
 workflow_path = node[:hadoop_smoke_tests][:hdfs_wf_path]
  
-ruby_block "collect_properties_data" do
-  block do
-    chef_env = node.environment
-    krb_realm = node[:bcpc][:hadoop][:kerberos][:realm]
-    resource_managers = node[:bcpc][:hadoop][:rm_hosts].map do |rms| float_host(rms.hostname) end
-    zookeeper_quorum = node[:bcpc][:hadoop][:zookeeper][:servers].map do |zks| float_host(zks.hostname) end
-    fs = "hdfs://#{chef_env}"
-    rm = if resource_managers.length > 1 then chef_env else resource_managers[0] end
-    thrift_uris = node[:bcpc][:hadoop][:hive_hosts]
-      .map { |s| 'thrift://' + float_host(s[:hostname]) + ':9083' }.join(",")
-    node.run_state['smoke'] = {}
-    node.run_state['smoke']['wf_path'] = workflow_path
-    node.run_state['smoke']['rm'] = rm
-    node.run_state['smoke']['fs'] = fs
-    node.run_state['smoke']['thrift_uris'] = thrift_uris
-    node.run_state['smoke']['krb_realm'] = krb_realm
-    node.run_state['smoke']['zk_quorum'] = zookeeper_quorum.join(",")
-  end
-end
+#ruby_block "collect_properties_data" do
+#  block do
+#    chef_env = node.environment
+#    krb_realm = node[:bcpc][:hadoop][:kerberos][:realm]
+#    resource_managers = node[:bcpc][:hadoop][:rm_hosts].map do |rms| float_host(rms.hostname) end
+#    zookeeper_quorum = node[:bcpc][:hadoop][:zookeeper][:servers].map do |zks| float_host(zks.hostname) end
+#    fs = "hdfs://#{chef_env}"
+#    rm = if resource_managers.length > 1 then chef_env else resource_managers[0] end
+#    thrift_uris = node[:bcpc][:hadoop][:hive_hosts]
+#      .map { |s| 'thrift://' + float_host(s[:hostname]) + ':9083' }.join(",")
+#    node.run_state['smoke'] = {}
+#    node.run_state['smoke']['wf_path'] = workflow_path
+#    node.run_state['smoke']['rm'] = rm
+#    node.run_state['smoke']['fs'] = fs
+#    node.run_state['smoke']['thrift_uris'] = thrift_uris
+#    node.run_state['smoke']['krb_realm'] = krb_realm
+#    node.run_state['smoke']['zk_quorum'] = zookeeper_quorum.join(",")
+#  end
+#end
 
 directory "#{Chef::Config['file_cache_path']}/oozie-smoke-test" do
 end
@@ -53,7 +53,6 @@ end
 template "#{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties" do
   source 'smoke_test_job_properties.erb'
   variables ( lazy {{smoke: node.run_state['smoke']}} )
-<<<<<<< HEAD
 end
 
 execute "create HDFS workflow path #{workflow_path}" do
@@ -66,30 +65,18 @@ end
 execute "upload workflow to #{workflow_path}" do
   command "hdfs dfs -copyFromLocal #{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties #{workflow_path}" 
   user test_user
-=======
->>>>>>> oozie job submits but fails
-end
-
-execute "create HDFS workflow path #{workflow_path}" do
-  command "hdfs dfs -mkdir -p #{workflow_path}"
-  user 'hdfs'
-  not_if "hdfs dfs -test #{workflow_path}"
 end
 
 execute "upload workflow to #{workflow_path}" do
   command "hdfs dfs -copyFromLocal -f #{Chef::Config['file_cache_path']}/oozie-smoke-test/workflow.xml #{workflow_path}/" 
   user 'hdfs'
-  #not_if "hdfs dfs -test #{workflow_path}/workflow.xml", user: 'hdfs'
 end
 
 Chef::Resource::RubyBlock.send(:include, HadoopSmokeTests::OozieHelper)
 
 ruby_block 'submit oozie smoke test' do
   block do
-<<<<<<< HEAD
     include HadoopSmokeTests::OozieHelper
-=======
->>>>>>> oozie job submits but fails
     submit_workflow_running_host(test_user, "#{Chef::Config['file_cache_path']}/oozie-smoke-test/smoke_test_job.properties")
   end
 end
