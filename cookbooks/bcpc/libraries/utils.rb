2cbd0836d65c0f67c2842282b5a6aba88ea80e1a
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-06-02 14:21:45 -0400

    need to sleect heads

--- cookbooks/bcpc/libraries/utils.rb
+++ cookbooks/bcpc/libraries/utils.rb
@@ -185,7 +185,7 @@ def get_head_nodes
   # Zookeeper nodes are the de facto heads for a Kafka cluster, since
   # they run Zabbix, Graphite, MySQL et al.
   #
-  fetch_cluster_def
+  fetch_cluster_def.select { |hst| hst[:runlist].include? "role[BCPC-Hadoop-Head]" or hst[:runlist].include "role[BCPC-Kafka-Head-Zookeeper]" }
 end
 
 def get_head_node_names
