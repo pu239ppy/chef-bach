f37369a3197ebcd89f8bb69f85e7ab4e545b915e
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-07-19 12:45:00 -0400

    include?

--- cookbooks/bcpc/libraries/utils.rb
+++ cookbooks/bcpc/libraries/utils.rb
@@ -187,7 +187,7 @@ def get_head_nodes
   # Zookeeper nodes are the de facto heads for a Kafka cluster, since
   # they run Zabbix, Graphite, MySQL et al.
   #
-  BACH::ClusterData.fetch_cluster_def.select { |hst| hst[:runlist].include? "role[BCPC-Hadoop-Head]" or hst[:runlist].include "role[BCPC-Kafka-Head-Zookeeper]" }
+  BACH::ClusterData.fetch_cluster_def.select { |hst| hst[:runlist].include? "role[BCPC-Hadoop-Head]" or hst[:runlist].include? "role[BCPC-Kafka-Head-Zookeeper]" }
 end
 
 def get_head_node_names
