4b40c592e06528ac88e63ec2e4d99b61bd8d6480
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-08-02 20:50:16 -0400

    instead of having two different modules called cluster_data just combine them into a gem that ca nbe used on the bootstrap and by the cookbook

--- lib/cluster-data-gem/lib/cluster_data.rb
+++ lib/cluster-data-gem/lib/cluster_data.rb
@@ -14,7 +14,6 @@ require 'ohai'
 require 'pry'
 require 'ridley'
 require 'faraday'
-require_relative '../cookbooks/bcpc/libraries/cluster_data'
 Ridley::Logging.logger.level = Logger.const_get 'ERROR'
 
 module BACH
@@ -150,13 +149,107 @@ module BACH
       raise "Did not find #{entry[:fqdn]} in Chef index after 180 seconds!"
     end
 
-    def repo_dir
-      # This file is in the 'lib' subdirectory, so the repo dir is its parent.
-      File.expand_path('..', File.dirname(__FILE__))
-    end
-
     def ridley
       @ridley ||= Dir.chdir(repo_dir) { Ridley.from_chef_config }
     end
+
+    def fqdn(entry)
+      if(entry[:dns_domain])
+        entry[:hostname] + '.' + entry[:dns_domain]
+      else
+        entry[:hostname]
+      end
+    end
+
+    def get_entry(name)
+      parse_cluster_txt.select do |ee|
+        ee[:hostname] == name || fqdn(ee) == name
+      end.first
+    end
+
+    def is_virtualbox_vm?(entry)
+      %r{^08:00:27}.match(entry[:mac_address])
+    end
+
+    def validate_node_number?(nn)
+      # node number must either be '-' or a positive integer 
+      # 1..255
+      if nn != '-' and nn.to_i < 1 and nn.to_i > 255 then
+        false
+      else
+        true
+      end 
+    end
+
+    def validate_cluster_def(cluster_def, fields)
+        cdef_copy = cluster_def.select{ |row| row[:runlist] != 'SKIP' }
+        # validate columns each row has the same number of fields as fields
+        faulty_rows = cdef_copy.select{ |row| row.length != fields.length }
+        if faulty_rows.length > 0  then
+          faulty_rows.each { |row| puts row }
+          fail "Retreived cluster data appears to be invalid -- missing columns"
+        end
+        # validate node ids 
+        if (cluster_def.select{ |row| validate_node_number?(row[:node_id]) == false }).length > 0  then
+          fail "Retreived cluster data appears to be invalid -- node IDs must be positive integers between 0 and 256 (1..255)"
+        end 
+    end
+
+    def parse_cluster_def(cluster_def)
+      # parse something that looks like cluster.txt and memorize the result
+      fields = [
+                :node_id,
+                :hostname,
+                :mac_address,
+                :ip_address,
+                :ilo_address,
+                :cobbler_profile,
+                :dns_domain,
+                :runlist
+               ]
+
+        # This is really gross because Ruby 1.9 lacks Array#to_h.
+        cdef = cluster_def.map do |line|
+          entry = Hash[*fields.zip(line.split(' ')).flatten(1)]
+          entry.merge({fqdn: fqdn(entry)})
+        end
+        # field size check will fail if we do not do this
+        fields += [:fqdn]
+        validate_cluster_def(cdef, fields)
+        cdef
+    end
+
+    # combines local cluster.txt access with http call to cluster data
+    def fetch_cluster_def
+        begin
+          fetch_cluster_def_http
+        rescue Exception => http_e
+          puts http_e 
+          puts http_e.btacktrace
+          fetch_cluster_def_local 
+        end
+    end
+
+    # fetch cluster definition via http
+    def fetch_cluster_def_http
+      cluster_def_url = "http://#{node[:bcpc][:bootstrap][:server]}#{node[:bcpc][:bootstrap][:cluster_def_path]}"
+      response = Faraday.get cluster_def_url 
+      if response.success? then
+        parse_cluster_def(response.body.split("\n"))
+      else
+        nil
+      end
+    end 
+      
+    # locally access cluster.txt
+    def fetch_cluster_def_local
+      parse_cluster_def(File.readlines(File.join(repo_dir, 'cluster.txt')))
+    end
+
+    def repo_dir
+      '/home/vagrant/chef-bcpc'
+    end
+
   end
 end
+
